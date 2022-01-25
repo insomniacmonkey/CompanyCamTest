52b8dc7991d993c17cdb8130ab1759dcd603665a
Chanse Strode <chansestrode@gmail.com>
2022-01-24 22:01:35 -0600

    Clean up and added more test
    - Cleaned up dup check.
    -Added get started redirect check.
    -Added Annual pricing button click and price change

--- cypress/integration/Pricing/pricing.tests.js
+++ cypress/integration/Pricing/pricing.tests.js
@@ -13,18 +13,31 @@ describe('Pro Pricing and Content', () => {
         cy.get('[class="btn btn--blue btn--small"]').contains("Get Started")
         cy.get('[class="pricing-table-plan-features"]').contains("250 (Billed Per User)")
         cy.get('[class="pricing-table-plan-features"]').contains("Unlimited Projects")
-        //The rest of the list would go here. 
-        cy.get('[id="faq"]').contains("Frequently Asked Questions")
-        //The rest of the FAQ
+        //I am sure there is an array or another approach we could do for the whole list of features. 
+
     })
 
+    it('Check that "Get Started" button redirects user to /signup.', () => {
+        cy.get('[class="btn btn--blue btn--small"]').contains("Get Started").click()
+        //This is not a great approach. We would want to add a custom QA id for the app.companycam.com/signup page
+        cy.contains("Create a new company.")
+
+    })
+
+    it('Check Annual Pricing', () => {
+        cy.visit('https://companycam.com/pricing')
+        cy.get('[class="tgl-btn"]').click()
+        cy.get('[class="pricing-table-plan-price pricing-table-plan-price--pro"]').contains("16")
+        
+    })
 
 })
+
 describe('FAQ', () => {
     it('Check FAQ Content.', () => {
+        cy.visit('https://companycam.com/pricing')
         cy.get('[id="faq"]').contains("Frequently Asked Questions")
         //The rest of the FAQ
     })
-
 
 })
